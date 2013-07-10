292647df7c2fcb2591094e4617057ea668abaefb
Paul Wood <paulwooddesign@gmail.com>
2013-07-09 23:00:12 -0600

    Added Missoin

---
+++ part-mission.php
@@ -0,0 +1,10 @@
+<!-- mission -->
+<section id="mission" class="clearfix">
+	<div class="site-width">
+		<div id="bannerText2" class="copy-width">
+			<h2><?php echo of_get_option('ttrust_banner_text_primary'); ?></h2>
+			<p><?php echo of_get_option('ttrust_banner_text_secondary'); ?></p>
+		</div>
+	</div>
+</section>
+<!-- /mission -->
